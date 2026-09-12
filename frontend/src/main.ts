83eb6182507488cecba512c95a14f405437dc74b
MiyamotoMusasshi <wowad506hp@gmail.com>
2026-09-12 08:56:16 +0200

    second commit

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,11 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
+import router from './router.ts'
 
-const app = createApp(App)
-
-app.use(createPinia())
-
-app.mount('#app')
+createApp(App).use(router).mount('#app')
